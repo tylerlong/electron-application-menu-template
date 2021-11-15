70e2ac1cbb0cbc0b89b0f35ba06e3667131fd478
Tyler Long <tyler4long@gmail.com>
2021-11-15 12:53:26 -0800

    First version

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,175 @@
 // eslint-disable-next-line node/no-unpublished-import
-import {MenuItemConstructorOptions} from 'electron';
+import {app, MenuItemConstructorOptions} from 'electron';
 
 export const newTemplate = (): MenuItemConstructorOptions[] => {
-  const template: MenuItemConstructorOptions[] = [];
+  const template: MenuItemConstructorOptions[] = [
+    {
+      label: 'File',
+      submenu: [
+        {
+          label: 'Close Window',
+          accelerator: 'CmdOrCtrl+W',
+          role: 'close',
+        },
+      ],
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          role: 'undo',
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          role: 'redo',
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          role: 'cut',
+        },
+        {
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          role: 'copy',
+        },
+        {
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          role: 'paste',
+        },
+        {
+          label: 'Paste and Match Style',
+          accelerator: 'Alt+Shift+CmdOrCtrl+V',
+          role: 'pasteAndMatchStyle',
+        },
+        {
+          label: 'Delete',
+          role: 'delete',
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          role: 'selectAll',
+        },
+      ],
+    },
+    {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          role: 'reload',
+        },
+        {
+          label: 'Toggle Developer Tools',
+          accelerator:
+            process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+          role: 'toggleDevTools',
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'Actual Size',
+          accelerator: 'CmdOrCtrl+0',
+          role: 'resetZoom',
+        },
+        {
+          label: 'Zoom In',
+          accelerator: 'CmdOrCtrl++',
+          role: 'zoomIn',
+        },
+        {
+          label: 'Zoom Out',
+          accelerator: 'CmdOrCtrl+-',
+          role: 'zoomOut',
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'Toggle Full Screen',
+          accelerator: process.platform === 'darwin' ? 'Ctrl+Command+F' : 'F11',
+          role: 'togglefullscreen',
+        },
+      ],
+    },
+    {
+      label: 'Window',
+      role: 'window',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'CmdOrCtrl+M',
+          role: 'minimize',
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'Bring All to Front',
+          role: 'front',
+        },
+      ],
+    },
+    {
+      label: 'Help',
+      role: 'help',
+    },
+  ];
+
+  if (process.platform === 'darwin') {
+    const {name} = app;
+    template.unshift({
+      label: name,
+      submenu: [
+        {
+          label: 'About ' + name,
+          role: 'about',
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'Services',
+          role: 'services',
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'Hide ' + name,
+          accelerator: 'Command+H',
+          role: 'hide',
+        },
+        {
+          label: 'Hide Others',
+          accelerator: 'Alt+Command+H',
+          role: 'hideOthers',
+        },
+        {
+          label: 'Show All',
+          role: 'unhide',
+        },
+        {
+          type: 'separator',
+        },
+        {
+          label: 'Quit ' + name,
+          accelerator: 'Command+Q',
+          role: 'quit',
+        },
+      ],
+    });
+  }
+
   return template;
 };
