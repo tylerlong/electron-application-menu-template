88a657e740538377e2003a638895cf78c7386918
Tyler Liu <tyler4long@gmail.com>
2023-02-23 17:27:39 -0800

    Try to simplify

--- src/index.ts
+++ src/index.ts
@@ -4,58 +4,36 @@ import {app, MenuItemConstructorOptions} from 'electron';
 export const newTemplate = (): MenuItemConstructorOptions[] => {
   const template: MenuItemConstructorOptions[] = [
     {
-      label: 'File',
-      submenu: [
-        {
-          label: 'Close Window',
-          accelerator: 'CmdOrCtrl+W',
-          role: 'close',
-        },
-      ],
+      role: 'fileMenu',
     },
     {
       label: 'Edit',
       submenu: [
         {
-          label: 'Undo',
-          accelerator: 'CmdOrCtrl+Z',
           role: 'undo',
         },
         {
-          label: 'Redo',
-          accelerator: 'Shift+CmdOrCtrl+Z',
           role: 'redo',
         },
         {
           type: 'separator',
         },
         {
-          label: 'Cut',
-          accelerator: 'CmdOrCtrl+X',
           role: 'cut',
         },
         {
-          label: 'Copy',
-          accelerator: 'CmdOrCtrl+C',
           role: 'copy',
         },
         {
-          label: 'Paste',
-          accelerator: 'CmdOrCtrl+V',
           role: 'paste',
         },
         {
-          label: 'Paste and Match Style',
-          accelerator: 'Alt+Shift+CmdOrCtrl+V',
           role: 'pasteAndMatchStyle',
         },
         {
-          label: 'Delete',
           role: 'delete',
         },
         {
-          label: 'Select All',
-          accelerator: 'CmdOrCtrl+A',
           role: 'selectAll',
         },
         {
@@ -65,22 +43,18 @@ export const newTemplate = (): MenuItemConstructorOptions[] => {
           label: 'Substitutions',
           submenu: [
             {
-              label: 'Show Substitutions',
               role: 'showSubstitutions',
             },
             {
               type: 'separator',
             },
             {
-              label: 'Smart Quotes',
               role: 'toggleSmartQuotes',
             },
             {
-              label: 'Smart Dashes',
               role: 'toggleSmartDashes',
             },
             {
-              label: 'Text Replacement',
               role: 'toggleTextReplacement',
             },
           ],
@@ -89,11 +63,9 @@ export const newTemplate = (): MenuItemConstructorOptions[] => {
           label: 'Speech',
           submenu: [
             {
-              label: 'Start Speaking',
               role: 'startSpeaking',
             },
             {
-              label: 'Stop Speaking',
               role: 'stopSpeaking',
             },
           ],
@@ -104,45 +76,30 @@ export const newTemplate = (): MenuItemConstructorOptions[] => {
       label: 'View',
       submenu: [
         {
-          label: 'Reload',
-          accelerator: 'CmdOrCtrl+R',
           role: 'reload',
         },
         {
-          label: 'Force Reload',
-          accelerator: 'Shift+CmdOrCtrl+R',
           role: 'forceReload',
         },
         {
-          label: 'Toggle Developer Tools',
-          accelerator:
-            process.platform === 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
           role: 'toggleDevTools',
         },
         {
           type: 'separator',
         },
         {
-          label: 'Actual Size',
-          accelerator: 'CmdOrCtrl+0',
           role: 'resetZoom',
         },
         {
-          label: 'Zoom In',
-          accelerator: 'CmdOrCtrl+Plus',
           role: 'zoomIn',
         },
         {
-          label: 'Zoom Out',
-          accelerator: 'CmdOrCtrl+-',
           role: 'zoomOut',
         },
         {
           type: 'separator',
         },
         {
-          label: 'Toggle Full Screen',
-          accelerator: process.platform === 'darwin' ? 'Ctrl+Command+F' : 'F11',
           role: 'togglefullscreen',
         },
       ],
@@ -152,27 +109,21 @@ export const newTemplate = (): MenuItemConstructorOptions[] => {
       role: 'window',
       submenu: [
         {
-          label: 'Minimize',
-          accelerator: 'CmdOrCtrl+M',
           role: 'minimize',
         },
         {
-          label: 'Zoom',
           role: 'zoom',
         },
         {
           type: 'separator',
         },
         {
-          label: 'Bring All to Front',
           role: 'front',
         },
       ],
     },
     {
-      label: 'Help',
       role: 'help',
-      submenu: [],
     },
   ];
 
@@ -182,39 +133,30 @@ export const newTemplate = (): MenuItemConstructorOptions[] => {
       label: name,
       submenu: [
         {
-          label: 'About ' + name,
           role: 'about',
         },
         {
           type: 'separator',
         },
         {
-          label: 'Services',
           role: 'services',
         },
         {
           type: 'separator',
         },
         {
-          label: 'Hide ' + name,
-          accelerator: 'Command+H',
           role: 'hide',
         },
         {
-          label: 'Hide Others',
-          accelerator: 'Alt+Command+H',
           role: 'hideOthers',
         },
         {
-          label: 'Show All',
           role: 'unhide',
         },
         {
           type: 'separator',
         },
         {
-          label: 'Quit ' + name,
-          accelerator: 'Command+Q',
           role: 'quit',
         },
       ],
